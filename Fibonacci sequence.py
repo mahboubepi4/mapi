c4f89dd06e92ceb46ee366cbcc8c4909b9b9801b
Mahboube Piryaee <108015227+mahboubepi4@users.noreply.github.com>
2022-08-15 16:13:49 +0430

    Add files via upload

---
+++ Fibonacci sequence.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 15 15:03:50 2022
+
+@author: Mahboobeh
+Fibonacci sequence
+"""
+a = 0
+b = 1
+while a < 1000:
+    print(a, end = " - ")
+    a , b = b , a + b
+
